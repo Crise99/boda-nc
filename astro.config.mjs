a46acb35719bce46218a07434ae602c2f9dffb41
vLorente <valenlj7@gmail.com>
2025-02-26 20:01:55 +0100

    Config Page has Static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import cloudflare from "@astrojs/cloudflare"
+import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+	integrations: [
+		mdx(),
+		react()
+	],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	output: "static",
+	adapter: cloudflare()
+})
