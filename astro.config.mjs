5a975f6a43b5b73e3c7243a5c157ab314c1d3ebf
vLorente <valenlj7@gmail.com>
2025-02-27 13:58:02 +0100

    Refactor styles and components: remove unused global SCSS, add global CSS, and update navigation structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,19 @@
 import cloudflare from "@astrojs/cloudflare"
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 import icon from "astro-icon"
 
+import tailwindcss from "@tailwindcss/vite"
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), react(), icon()],
+	output: "static",
+	adapter: cloudflare(),
 
 	vite: {
-		plugins: [tailwindcss()],
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: "modern-compiler",
-				},
-			},
-		},
-	},
-	output: "static",
-	adapter: cloudflare()
+		plugins: [tailwindcss()]
+	}
 })
