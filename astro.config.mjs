c71d79644f579996b98953fd8a0dcf57f0a0b30d
vLorente <valenlj7@gmail.com>
2025-03-06 20:28:15 +0100

    Refactor astro.config.mjs to set output to "static" and configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,24 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import compress from "@playform/compress";
 import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
-import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
+import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel";
-
-// https://astro.build/config
 export default defineConfig({
 	site: "https://horizon.cosmicthemes.com",
-
+	output: "static",
+	adapter: vercel(),
 	integrations: [
-		// example auto import component into blog post mdx files
 		AutoImport({
 			imports: [
-				// https://github.com/delucis/astro-auto-import
 				"@components/Admonition/Admonition.astro",
 			],
 		}),
 		mdx(),
 		icon({
-			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
-			// https://www.astroicon.dev/reference/configuration#include
 			include: {
 				tabler: [
 					"bulb",
@@ -46,19 +41,15 @@ export default defineConfig({
 			HTML: true,
 			JavaScript: true,
 			CSS: true,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
+			Image: false,
+			SVG: false,
 		}),
 	],
 
 	vite: {
 		plugins: [tailwindcss()],
-		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
 		build: {
 			assetsInlineLimit: 0,
 		},
-	},
-
-	output: "static",
-	adapter: vercel()
+	}
 });
