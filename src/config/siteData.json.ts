906e9ca0eba74aad1aa02518bb0dace92ed87604
Crise99 <cristian.s.arana@gmail.com>
2025-03-19 13:11:53 +0100

    Update default image reference in site data

--- src/config/siteData.json.ts
+++ src/config/siteData.json.ts
@@ -61,7 +61,7 @@ const siteData: SiteDataProps = {
 
 	// default image for meta tags if the page doesn't have an image already
 	defaultImage: {
-		src: "/images/invitacion.jpeg",
+		src: "/images/invitacion2.jpeg",
 		alt: "Nuria & Cristian",
 	},
 
