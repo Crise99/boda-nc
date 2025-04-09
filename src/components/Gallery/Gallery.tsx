da57a5ea18d816c3d778f38666adc88d50785b72
vLorente <valenlj7@gmail.com>
2025-04-09 15:26:04 +0200

    Feature: Refactor Gallery component to use Title component and update loading strategy

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,10 +1,9 @@
-export const prerender = false; // Not needed in 'server' mode
-
 import "photoswipe/style.css";
 import "@components/Gallery/styles/Gallery.css";
 import Button from "@components/Gallery/Button";
+import Title from "@components/Gallery/Title";
 import { useGallery } from "@components/Gallery/hooks/useGallery";
-import "@/styles/global.css";
+// import "@/styles/global.css";
 
 import { type Masonry } from "./types/gallery";
 
@@ -13,11 +12,7 @@ export default function Gallery() {
 
 	return (
 		<section className="max-w-8xl mx-auto px-5 py-20 md:px-20 md:pt-30">
-			<h2 className="mx-auto mb-8 text-center text-3xl font-semibold tracking-wide lg:text-6xl">
-				Galería
-			</h2>
-			<p className="text-center text-2xl">Todas las fotos de nuestra boda</p>
-
+			<Title title="Galería" subtitle="Todas las fotos de nuestra boda" />
 			<masonry-layout gap="24" maxColWidth="600" className="mx-4 py-20 lg:mx-auto" id="gallery">
 				{photos.map(({ height, width }, index) => (
 					<a
@@ -34,7 +29,7 @@ export default function Gallery() {
 							className="h-auto w-full rounded-xl object-cover"
 							loading="lazy"
 							src={`/gallery/thumbnails/img-${index + 1}.webp`}
-							alt="Fotografía de los de la boda de Nuria y Cristian"
+							alt="Fotografía de la boda de Nuria y Cristian"
 						/>
 						<img
 							className="absolute inset-0 -z-10 object-cover opacity-0 blur-md contrast-150 transition group-hover:opacity-100"
